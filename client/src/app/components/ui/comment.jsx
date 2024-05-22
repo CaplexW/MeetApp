9b5fb0f53dc82340e92c66492b35aaeb7bf03fcd
Caplex <caplex.work@gmail.com>
2024-05-22 23:43:27 +0700

    lint changes

--- client/src/app/components/ui/comment.jsx
+++ client/src/app/components/ui/comment.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
