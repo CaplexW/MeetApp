d0856f6be82738621d13906dae634765a2aaf0d4
Caplex <caplex.work@gmail.com>
2024-05-31 13:33:50 +0700

    small adjustments and visual fixes

--- client/src/app/layouts/main.jsx
+++ client/src/app/layouts/main.jsx
@@ -33,12 +33,12 @@ export default function MainPage() {
 
   if (isLogged) return <Link to="/users"><button className="btn btn-primary m-5" type="button">Найти компанию!</button></Link>;
   return (
-    <div className="row gutter-sm p-3 d-flex justify-content-center align-items-center">
-      {/* <h1 className="mx-5 my-3">Найди себе компанию!</h1> */}
-      <div className="m-5 align-items-center">
-        <Link to="/login"><button className="btn btn-primary col-sm-3 m-1" type="button">Войти</button></Link>
-        <Link to="/login/register"><button className="btn btn-primary col-sm-3 m-1" type="button">Зарегестрироваться</button></Link>
-        <button className="btn btn-info col-sm-3 m-1" disabled={clicked} onClick={activeGuestMode} type="button">Осмотреться</button>
+    <div className="row gutter-md p-3">
+      <h1 className="mx-5 my-3">Найди себе компанию!</h1>
+      <div className="m-5">
+        <Link to="/login"><button className="btn btn-primary col-md-1 m-1" type="button">Войти</button></Link>
+        <Link to="/login/register"><button className="btn btn-primary col-md-3 m-1" type="button">Зарегестрироваться</button></Link>
+        <button className="btn btn-info col-md-2 m-1" disabled={clicked} onClick={activeGuestMode} type="button">Осмотреться</button>
       </div>
     </div>
   );
